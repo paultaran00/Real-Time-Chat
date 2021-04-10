73e24b636eb6d4a3919ccd0311188a3e467bf5ea
Paul Taran <58374574+paultaran00@users.noreply.github.com>
2021-04-10 19:06:53 +0300

    Authentication 0.2
    Front-end 90%

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
-var qs = require("querystring");
 var session = require('express-session');
+const http = require('http').createServer(app);
 const url = require('url');
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+var io = require('socket.io');
 const mongo = require("mongodb");
+
 
 app.use(express.static(__dirname + '/public'));
 app.use("/public", express.static('./public/'));
+
+
+
 
 app.get('/', function(request, response){
     response.sendFile(__dirname + '/public/html/login.html');
--- public/js/login.js
+++ public/js/login.js
@@ -1,19 +1,58 @@
-$('.message .create').click(function(){
-    $('.register-form').animate({height: "toggle", opacity: "toggle"}, "slow");
-    $('.login-form').animate({height: "toggle", opacity: "toggle"}, "slow");
+//animations
+
+$(document).ready(function() {
+    $('.message .create').click(function(){
+        $('.register-form').animate({height: "toggle", opacity: "toggle"}, "slow");
+        $('.login-form').animate({height: "toggle", opacity: "toggle"}, "slow");
+    });
+    
+    $('.message .login').click(function(){
+        $('.login-form').animate({height: "toggle", opacity: "toggle"}, "slow");
+        $('.register-form').animate({height: "toggle", opacity: "toggle"}, "slow");
+    });
+    
+    $('.message .change').click(function(){
+        $('.login-form').animate({height: "toggle", opacity: "toggle"}, "slow");
+        $('.changepass-form').animate({height: "toggle", opacity: "toggle"}, "slow");
+    });
+    
+    $('.message .clogin').click(function(){
+        $('.login-form').animate({height: "toggle", opacity: "toggle"}, "slow");
+        $('.changepass-form').animate({height: "toggle", opacity: "toggle"}, "slow");
+    });
+
 });
 
-$('.message .login').click(function(){
-    $('.login-form').animate({height: "toggle", opacity: "toggle"}, "slow");
-    $('.register-form').animate({height: "toggle", opacity: "toggle"}, "slow");
-});
 
-$('.message .change').click(function(){
-    $('.login-form').animate({height: "toggle", opacity: "toggle"}, "slow");
-    $('.changepass-form').animate({height: "toggle", opacity: "toggle"}, "slow");
-});
 
-$('.message .clogin').click(function(){
-    $('.login-form').animate({height: "toggle", opacity: "toggle"}, "slow");
-    $('.changepass-form').animate({height: "toggle", opacity: "toggle"}, "slow");
-});
+//check validation
+function checkReg() {
+    var regform = $('.register-form');
+    console.log(regform[0].checkValidity());
+    if(regform[0].checkValidity()) {
+        $('.createbut').click(function(){
+            $(".register-form").css("display", "none");
+            $(".login-form").css("display", "block");
+        });
+    };
+};
+
+function checkChpas(){
+    var regform2 = $('.changepass-form');
+    console.log(regform2[0].checkValidity());
+    if(regform2[0].checkValidity()) {
+        $('.changepassbut').click(function(){
+            $(".changepass-form").css("display", "none");
+            $(".login-form").css("display", "block");
+        });
+    };
+};
+
+
+
+// var regform3 = $('login-form');
+// if(regform3[0].checkValidity()) {
+//     $('loginbut').click(function(){
+//         window.location='http://www.google.com';
+//     });
+// };
